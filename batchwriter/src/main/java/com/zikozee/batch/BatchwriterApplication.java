6fa8290d8470dca8e1870833e22afb7099b10d88
ezekiel.eromosei <ezekiel.eromosei@longbridgetech.com>
2021-09-04 10:57:47 +0100

    implement item file writer closes #7

---
+++ batchwriter/src/main/java/com/zikozee/batch/BatchwriterApplication.java
@@ -0,0 +1,13 @@
+package com.zikozee.batch;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BatchwriterApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BatchwriterApplication.class, args);
+    }
+
+}
